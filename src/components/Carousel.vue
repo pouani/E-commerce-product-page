76fc4e27f5766bd721c25faef4e676ea1e605de7
Ulrich POUANI <pouani@medicasure.com>
2022-12-02 19:45:06 +0100

    create caroussel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,23 +1,30 @@
 <template lang="">
     <div>
-        <div class="image-box">
-            <img src="@/assets/images/icon-previous.svg" alt="prev" class="previous" @click="plusSlides(-1)">
-            <img src="@/assets/images/image-product-1.jpg" alt="product-1" class="slides">
-            <img src="@/assets/images/image-product-2.jpg" alt="product-2" class="slides">
-            <img src="@/assets/images/image-product-3.jpg" alt="product-3" class="slides">
-            <img src="@/assets/images/image-product-4.jpg" alt="product-4" class="slides">
-             <img src="@/assets/images/icon-next.svg" alt="next" class="next" @click="plusSlides(1)">
-            <div class="thumbnail-wrapper">
-                <div class="thumbnails" @click="currentSlide(1)"><img src="@/assets/images/image-product-1-thumbnail.jpg" alt="thumbnail-1"></div>
-                <div class="thumbnails" @click="currentSlide(2)"><img src="@/assets/images/image-product-2-thumbnail.jpg" alt="thumbnail-2"></div>
-                <div class="thumbnails" @click="currentSlide(3)"><img src="@/assets/images/image-product-3-thumbnail.jpg" alt="thumbnail-3"></div>
-                <div class="thumbnails" @click="currentSlide(4)"><img src="@/assets/images/image-product-4-thumbnail.jpg" alt="thumbnail-4"></div>
+        <div class="caroussel">
+            <img src="@/assets/images/icon-previous.svg" alt="prev" class="caroussel-btn previous md:hidden" @click="plusSlides(-1)">
+            <div class="rounded-xl overflow-hidden mb-3">
+                <img src="@/assets/images/image-product-1.jpg" alt="product-1" class="slides">
+                <img src="@/assets/images/image-product-2.jpg" alt="product-2" class="slides">
+                <img src="@/assets/images/image-product-3.jpg" alt="product-3" class="slides">
+                <img src="@/assets/images/image-product-4.jpg" alt="product-4" class="slides">
             </div>
+             <img src="@/assets/images/icon-next.svg" alt="next" class="caroussel-btn next md:hidden" @click="plusSlides(1)">
+            <ul class="thumbnail-wrapper flex gap-6 list-none">
+                <li class="thumbnails" @click="currentSlide(1)"><img src="@/assets/images/image-product-1-thumbnail.jpg" alt="thumbnail-1"></li>
+                <li class="thumbnails" @click="currentSlide(2)"><img src="@/assets/images/image-product-2-thumbnail.jpg" alt="thumbnail-2"></li>
+                <li class="thumbnails" @click="currentSlide(3)"><img src="@/assets/images/image-product-3-thumbnail.jpg" alt="thumbnail-3"></li>
+                <li class="thumbnails" @click="currentSlide(4)"><img src="@/assets/images/image-product-4-thumbnail.jpg" alt="thumbnail-4"></li>
+            </ul>
         </div>
     </div>
 </template>
 <script>
 export default {
+    data (){
+        return {
+            slideIndex: 1,
+        }
+    },
     methods: {
         plusSlides(n){
             this.showSlides(this.slideIndex += n);
@@ -43,7 +50,41 @@ export default {
           dots[this.slideIndex-1].className += " active";
         }
     },
+    mounted() {
+         this.showSlides(this.slideIndex);
+    },
 }
 </script>
 <style >
+.thumbnails{
+    border-radius: .6rem;
+    overflow: hidden;
+}
+.thumbnails:hover{
+  cursor: pointer;
+  opacity: 0.5;
+}
+.active.thumbnails{
+    opacity: 0.3;
+}
+.slides {
+  display: none;
+}
+
+.slides img {
+    height: auto;
+    border-radius: 5px;
+}
+
+.caroussel{
+    max-width: 35rem;
+}
+
+/* .caroussel-btn{
+    position: absolute;
+    top: 30%;
+}
+.next{
+    left: 40rem;
+} */
 </style>
